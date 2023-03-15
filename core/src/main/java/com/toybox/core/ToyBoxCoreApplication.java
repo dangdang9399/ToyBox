19f4941dc55235ce57e585c25f8436188b14e3d0
kimdaeun <07de19@gmail.com>
2023-03-15 20:56:30 +0900

    설정 추가

--- core/src/main/java/com/toybox/core/ToyBoxCoreApplication.java
+++ core/src/main/java/com/toybox/core/ToyBoxCoreApplication.java
@@ -1,4 +1,11 @@
 package com.toybox.core;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.toybox"})
 public class ToyBoxCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ToyBoxCoreApplication.class, args);
+    }
 }
