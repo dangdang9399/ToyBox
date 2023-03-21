40e6fa899af9a6be057a1095a491719cd297c622
kimdaeun <07de19@gmail.com>
2023-03-21 22:17:35 +0900

    properties 설정 추가

---
+++ core/src/main/java/com/toybox/core/model/ToyBoxProperties.java
@@ -0,0 +1,13 @@
+package com.toybox.core.model;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "toybox")
+@Data
+public class ToyBoxProperties {
+}
